eadcab0cd05a6c5ca68c4ac1741844e9ee908723
SMEZ1234 <sean.zammit@bigpond.com>
2014-09-04 21:23:02 +1000

    Testing edit in Netbeans Removed comment from Autonomous.java

--- base/commands/autonomous/Autonomous.java
+++ base/commands/autonomous/Autonomous.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package edu.wpi.first.wpilibj.base.commands.autonomous;
 
 import edu.wpi.first.wpilibj.superclasses.CommandBaseRedCrusade;
