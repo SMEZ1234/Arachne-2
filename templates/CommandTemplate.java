7f2d17e810c25b3b7402df83f8a6db3f526dc79d
SMEZ1234 <smez12345@gmail.com>
2014-09-04 21:36:01 +1000

    Removed unnecessary comments Added an import for the RobotMap in CommandTemplate.java

--- templates/CommandTemplate.java
+++ templates/CommandTemplate.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.base.RobotMap;
 import edu.wpi.first.wpilibj.superclasses.CommandBaseRedCrusade;
 
 /**
