41b3607af18ebc629e436551f2ab0f95acdc4141
SMEZ1234 <smez12345@gmail.com>
2014-10-14 07:44:41 +1100

    Added MotorGroups Not fully implemented, just an idea at the moment

---
+++ base/motors/MotorGroup.java
@@ -0,0 +1,47 @@
+package edu.wpi.first.wpilibj.base.motors;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.helpers.ArrayHelper;
+
+/**
+ *
+ * @author Sean Zammit
+ */
+public class MotorGroup { 
+    
+    public SafePWM[] motors = new SafePWM[0];
+    public float speed = 0;
+    public float position = 0;
+    public boolean requiresUpdate = false;
+    
+    public MotorGroup() {}
+    
+    public MotorGroup(float pos) {
+        position = pos;
+    }
+    
+    public MotorGroup addMotor(SafePWM motor) {
+        motors = (SafePWM[]) ArrayHelper.combineLists(motors, motor);        
+        return this;
+    }
+    
+    public MotorGroup setSpeed(float sp) {
+        speed = sp;
+        return this;
+    }
+    
+    public MotorGroup setPosition(float pos) {
+        position = pos;
+        return this;
+    }
+    
+    public MotorGroup setToUpdate() {
+        requiresUpdate = true;
+        return this;
+    }
+    
+    public MotorGroup setToUpdate(float pos) {
+        position = pos;
+        return setToUpdate();
+    }
+}
