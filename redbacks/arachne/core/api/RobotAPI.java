e73117a7c828caeec95b01f90487742c102e6d73
SMEZ1234 <smez12345@gmail.com>
2018-01-02 22:14:48 +1100

    Added API javadoc

--- redbacks/arachne/core/api/RobotAPI.java
+++ redbacks/arachne/core/api/RobotAPI.java
@@ -2,15 +2,37 @@ package redbacks.arachne.core.api;
 
 import java.util.ArrayList;
 
+/**
+ * Contains functions used to integrate secondary Arachne modules without overriding code in the core module.
+ * Though teams are welcome to use its functionality, you can get the same results by just extending and overriding the relevant classes.
+ *
+ * @author Sean Zammit
+ */
 public class RobotAPI
 {
+	/** A list of {@link RobotExtender RobotExtenders} to have their initialisation and execution methods called before user-created robot code. */
 	private static ArrayList<RobotExtender> preList = new ArrayList<RobotExtender>();
+	
+	/** A list of {@link RobotExtender RobotExtenders} to have their initialisation and execution methods called after user-created robot code. */
 	private static ArrayList<RobotExtender> postList = new ArrayList<RobotExtender>();
 	
+	/**
+	 * An enum used to determine when a {@link RobotExtender} is called.
+	 * PRE will be called before user-created code in a robot file extending {@link ArachneRobot}.
+	 * POST will be called after user-created code in a robot file extending {@link ArachneRobot}.
+	 *
+	 * @author Sean Zammit
+	 */
 	public static enum Order {
 		PRE, POST
 	}
 	
+	/**
+	 * Adds a {@link RobotExtender} to the relevant list to be called during initialisation and execution.
+	 *
+	 * @param extension The instance of a class overriding RobotExtender to be called.
+	 * @param order When the extension should be called. See {@link Order}.
+	 */
 	public static void addExtension(RobotExtender extension, Order order) {
 		switch(order) {
 			case POST:
@@ -22,6 +44,12 @@ public class RobotAPI
 		}
 	}
 	
+	/**
+	 * Gets a list of {@link RobotExtender RobotExtenders} to have their initialisation and execution methods called by {@link ArachneRobot}.
+	 *
+	 * @param order The current stage of execution. See {@link Order}.
+	 * @return The relevant list of RobotExtenders.
+	 */
 	public static ArrayList<RobotExtender> getExtensions(Order order) {
 		switch(order) {
 			case POST:
--- redbacks/arachne/core/api/RobotExtender.java
+++ redbacks/arachne/core/api/RobotExtender.java
@@ -1,31 +1,43 @@
 package redbacks.arachne.core.api;
 
+import redbacks.arachne.core.ArachneRobot;
 import redbacks.arachne.core.api.RobotAPI.Order;
 
+/**
+ * This is the superclass that enables other modules to add to the functionality of {@link ArachneRobot} without overriding either it or the user-created Robot class.
+ * Though teams are welcome to use its functionality, you can get the same results by just extending ArachneRobot.
+ *
+ * @author Sean Zammit
+ */
 public class RobotExtender
 {
+	/**
+	 * Superconstructor for classes adding to {@link ArachneRobot}'s functionality. Adds the class to a list to be checked for execution.
+	 *
+	 * @param order When the class's methods should be run. See {@link RobotAPI.Order}.
+	 */
 	public RobotExtender(Order order) {
 		RobotAPI.addExtension(this, order);
 	}
 	
-	/** Called once when the robot code starts. Replacement for {@link #robotInit() robotInit}. */
+	/** Called once when the robot code starts. Replacement for {@link ArachneRobot#robotInit()}. */
 	public void initialiseRobot() {}
 
-	/** Called once when autonomous starts. Replacement for {@link #autonomousInit() autonomousInit}. */
+	/** Called once when autonomous starts. Replacement for {@link ArachneRobot#autonomousInit()}. */
 	public void initialiseAuto() {}
 
-	/** Called repeatedly during autonomous. Replacement for {@link #autonomousPeriodic() autonomousPeriodic}. */
+	/** Called repeatedly during autonomous. Replacement for {@link ArachneRobot#autonomousPeriodic()}. */
 	public void executeAuto() {}
 
-	/** Called once when teleop starts. Replacement for {@link #teleopInit() teleopInit}. */
+	/** Called once when teleop starts. Replacement for {@link ArachneRobot#teleopInit()}. */
 	public void initialiseTeleop() {}
 
-	/** Called repeatedly during teleop. Replacement for {@link #teleopPeriodic() teleopPeriodic}. */
+	/** Called repeatedly during teleop. Replacement for {@link ArachneRobot#teleopPeriodic()}. */
 	public void executeTeleop() {}
 
-	/** Called once when the robot is disabled. Replacement for {@link #disabledInit() disabledInit}. */
+	/** Called once when the robot is disabled. Replacement for {@link ArachneRobot#disabledInit()}. */
 	public void initialiseDisabled() {}
 
-	/** Called repeatedly while the robot is disabled. Replacement for {@link #disabledPeriodic() disabledPeriodic}. */
+	/** Called repeatedly while the robot is disabled. Replacement for {@link ArachneRobot#disabledPeriodic()}. */
 	public void executeDisabled() {}
 }
