28020030db4a07d42f3ec2dcd533a3c92172560a
SMEZ1234 <smez12345@gmail.com>
2018-01-16 13:23:56 +1100

    Changed superclass of CtrlDrivetrain
    RobotDrive is now deprecated, and so CtrlDrivetrain now extends DifferentialDrive. Mecanum drive support is yet to be added.

--- redbacks/arachne/lib/motors/CtrlDrivetrain.java
+++ redbacks/arachne/lib/motors/CtrlDrivetrain.java
@@ -2,19 +2,24 @@ package redbacks.arachne.lib.motors;
 
 import java.util.ArrayList;
 
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import redbacks.arachne.core.ArachneRobot;
 
 /**
  * A replacement for {@link RobotDrive} that allows motor speeds to be set individually without having conflicting instructions from the RobotDrive methods.
+ * TODO Mecanum drive support
  * 
  * @author Sean Zammit
  */
-public class CtrlDrivetrain extends RobotDrive
+public class CtrlDrivetrain extends DifferentialDrive
 {
 	/** Used by Arachne to send speed values from all instances of CtrlDrivetrain to the drive motors. */
 	public static ArrayList<CtrlDrivetrain> drivetrains = new ArrayList<CtrlDrivetrain>();
 
+	public SpeedController m_leftMotor;
+	public SpeedController m_rightMotor;
+	
 	/**
 	 * Constructor for a drivetrain that has two motors, both of which must be instances of {@link CtrlDrive}.
 	 * 
@@ -23,19 +28,8 @@ public class CtrlDrivetrain extends RobotDrive
 	 */
 	public CtrlDrivetrain(CtrlDrive leftMotor, CtrlDrive rightMotor) {
 		super(leftMotor, rightMotor);
-		drivetrains.add(this);
-	}
-
-	/**
-	 * Constructor for a drivetrain that has four motors, all of which must be instances of {@link CtrlDrive}.
-	 * 
-	 * @param frontLeftMotor The front left CtrlDrive object used to drive the robot.
-	 * @param rearLeftMotor The rear left CtrlDrive object used to drive the robot.
-	 * @param frontRightMotor The front right CtrlDrive object used to drive the robot.
-	 * @param rearRightMotor The rear right CtrlDrive object used to drive the robot.
-	 */
-	public CtrlDrivetrain(CtrlDrive frontLeftMotor, CtrlDrive rearLeftMotor, CtrlDrive frontRightMotor, CtrlDrive rearRightMotor) {
-		super(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+		this.m_leftMotor = leftMotor;
+		this.m_rightMotor = rightMotor;
 		drivetrains.add(this);
 	}
 
@@ -43,15 +37,8 @@ public class CtrlDrivetrain extends RobotDrive
 	 * Called by Arachne while the robot is enabled on all instances of CtrlDrivetrain to send speed values to the drive motors.
 	 */
 	public void passControllerOutputs() {
-		if(m_frontLeftMotor != null) {
-			((CtrlDrive) m_frontLeftMotor).controller.set(m_frontLeftMotor.get() * m_maxOutput);
-		}
-		((CtrlDrive) m_rearLeftMotor).controller.set(m_rearLeftMotor.get() * m_maxOutput);
-
-		if(m_frontRightMotor != null) {
-			((CtrlDrive) m_frontRightMotor).controller.set(m_frontRightMotor.get() * m_maxOutput);
-		}
-		((CtrlDrive) m_rearRightMotor).controller.set(m_rearRightMotor.get() * m_maxOutput);
+		((CtrlDrive) m_leftMotor).controller.set(m_leftMotor.get() * m_maxOutput);
+		((CtrlDrive) m_rightMotor).controller.set(m_rightMotor.get() * m_maxOutput);
 
 		if(ArachneRobot.isIndivDriveControl) {
 			if(m_safetyHelper != null) m_safetyHelper.feed();
