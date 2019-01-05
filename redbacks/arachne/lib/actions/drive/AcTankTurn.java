b110c3861d293ca2ac8115e9549ea868c5b8babd
SMEZ1234 <smez12345@gmail.com>
2019-01-05 22:14:38 +1100

    Renamed parameter in AcTankTurn

--- redbacks/arachne/lib/actions/drive/AcTankTurn.java
+++ redbacks/arachne/lib/actions/drive/AcTankTurn.java
@@ -33,8 +33,8 @@ public class AcTankTurn extends Action
 	 * 
 	 * @param heading The desired heading.
 	 */
-	public AcTankTurn(double angleTarget) {
-		this(angleTarget, 1);
+	public AcTankTurn(double heading) {
+		this(heading, 1);
 	}
 	
 	public void onStart() {
